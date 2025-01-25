5efd73cfe5169eedb2d8e2c453844e4d4eab0dfa
Gabriel <7272gabriel@gmail.com>
2025-01-25 15:10:17 -0300

    added elementary main ui divisions

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+
+class Todos {
+
+    constructor(title, description, dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    renderTodos() {
+        const tasks = document.querySelector(".tasks") 
+    }
+}
